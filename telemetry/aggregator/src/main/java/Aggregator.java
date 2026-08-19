8393bcb84ac0837adcbc4bb9e288343202ddf6de
SergeyKudrin84 <conan4@yandex.ru>
2026-08-19 12:16:29 +1000

    выполняю задание

--- telemetry/aggregator/src/main/java/Aggregator.java
+++ telemetry/aggregator/src/main/java/Aggregator.java
@@ -1,9 +1,19 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import service.AggregationStarter;
 
 @SpringBootApplication
+@ComponentScan(value = {"service", "kafka"})
 public class Aggregator {
     public static void main(String[] args) {
-        SpringApplication.run(Aggregator.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(
+                        Aggregator.class,
+                        args
+                );
+
+        AggregationStarter starter = context.getBean(AggregationStarter.class);
+        starter.start();
     }
 }
