32dd9ecbef06ba9338f7a5603aa2e2fc3cfc85d3
SergeyKudrin84 <conan4@yandex.ru>
2026-08-20 11:59:21 +1000

    выполняю задание

--- telemetry/analyzer/src/main/java/analyzer/grpc/HubRouterClient.java
+++ telemetry/analyzer/src/main/java/analyzer/grpc/HubRouterClient.java
@@ -1,8 +1,14 @@
 package analyzer.grpc;
 
+
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.ActionTypeProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceActionProto;
+import ru.yandex.practicum.grpc.telemetry.event.DeviceActionRequestProto;
 import ru.yandex.practicum.grpc.telemetry.hubrouter.HubRouterControllerGrpc;
+
+import java.time.Instant;
 
 @Component
 public class HubRouterClient {
@@ -14,5 +20,31 @@ public class HubRouterClient {
             HubRouterControllerGrpc.HubRouterControllerBlockingStub client
     ) {
         this.client = client;
+    }
+
+    public void sendAction(
+            String hubId,
+            String scenarioName,
+            String sensorId,
+            String actionType,
+            Integer value,
+            Instant timestamp
+    ) {
+        DeviceActionProto.Builder actionBuilder = DeviceActionProto.newBuilder()
+                .setSensorId(sensorId)
+                .setType(ActionTypeProto.valueOf(actionType));
+
+        if (value != null) {
+            actionBuilder.setValue(value);
+        }
+
+        DeviceActionRequestProto request = DeviceActionRequestProto.newBuilder()
+                .setHubId(hubId)
+                .setScenarioName(scenarioName)
+                .setAction(actionBuilder.build())
+                //.setTimestamp(Timestamps.fromMillis(timestamp.toEpochMilli()))
+                .build();
+
+        client.handleDeviceAction(request);
     }
 }
--- telemetry/analyzer/src/main/java/analyzer/kafka/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/analyzer/kafka/SnapshotProcessor.java
@@ -1,11 +1,18 @@
 package analyzer.kafka;
 
+import analyzer.model.Condition;
+import analyzer.model.ConditionOperation;
+import analyzer.model.ConditionType;
+import analyzer.model.Scenario;
+import analyzer.repository.ScenarioRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
+import ru.yandex.practicum.kafka.telemetry.event.ClimateSensorAvro;
+import ru.yandex.practicum.kafka.telemetry.event.*;
+
 
 import java.time.Duration;
 import java.util.Collections;
@@ -18,9 +25,13 @@ import static kafka.KafkaTopics.SNAPSHOTS;
 public class SnapshotProcessor {
 
     private final KafkaConsumer<String, SensorsSnapshotAvro> consumer;
+    private final ScenarioRepository scenarioRepository;
 
-    public SnapshotProcessor(@Qualifier("snapshotConsumerProperties") Properties properties) {
+    public SnapshotProcessor(@Qualifier("snapshotConsumerProperties") Properties properties,
+                             ScenarioRepository scenarioRepository) {
+
         this.consumer = new KafkaConsumer<>(properties);
+        this.scenarioRepository = scenarioRepository;
     }
 
     public void start() {
@@ -47,5 +58,121 @@ public class SnapshotProcessor {
                 "Received snapshot: hubId={}",
                 snapshot.getHubId()
         );
+
+        var scenarios = scenarioRepository.findByHubId(snapshot.getHubId());
+
+        scenarios.stream()
+                .filter(scenario -> matches(scenario, snapshot))
+                .forEach(scenario ->
+                        log.info(
+                                "Scenario '{}' matches snapshot of hub {}",
+                                scenario.getName(),
+                                snapshot.getHubId()
+                        )
+                );
+    }
+
+    private boolean matches(Scenario scenario, SensorsSnapshotAvro snapshot) {
+        return scenario.getConditions().entrySet().stream()
+                .allMatch(entry -> {
+                    String sensorId = entry.getKey();
+                    Condition condition = entry.getValue();
+
+                    SensorStateAvro sensorState = snapshot.getSensorsState().get(sensorId);
+
+                    if (sensorState == null) {
+                        return false;
+                    }
+
+                    Object actualValue = extractValue(sensorState, condition.getType());
+
+                    return compare(
+                            actualValue,
+                            condition.getValue(),
+                            condition.getOperation()
+                    );
+                });
+    }
+
+    private Object extractValue(
+            SensorStateAvro sensorState,
+            ConditionType type
+    ) {
+        return switch (sensorState.getData()) {
+            case ClimateSensorAvro data -> switch (type) {
+                case TEMPERATURE -> data.getTemperatureC();
+                case HUMIDITY -> data.getHumidity();
+                case CO2LEVEL -> data.getCo2Level();
+                default -> throw new IllegalArgumentException(
+                        "Condition " + type + " is not supported by ClimateSensorAvro"
+                );
+            };
+
+            case LightSensorAvro data -> {
+                if (type != ConditionType.LUMINOSITY) {
+                    throw new IllegalArgumentException(
+                            "Condition " + type + " is not supported by LightSensorAvro"
+                    );
+                }
+
+                yield data.getLuminosity();
+            }
+
+            case MotionSensorAvro data -> {
+                if (type != ConditionType.MOTION) {
+                    throw new IllegalArgumentException(
+                            "Condition " + type + " is not supported by MotionSensorAvro"
+                    );
+                }
+
+                yield data.getMotion();
+            }
+
+            case SwitchSensorAvro data -> {
+                if (type != ConditionType.SWITCH) {
+                    throw new IllegalArgumentException(
+                            "Condition " + type + " is not supported by SwitchSensorAvro"
+                    );
+                }
+
+                yield data.getState();
+            }
+
+            case TemperatureSensorAvro data -> {
+                if (type != ConditionType.TEMPERATURE) {
+                    throw new IllegalArgumentException(
+                            "Condition " + type + " is not supported by TemperatureSensorAvro"
+                    );
+                }
+
+                yield data.getTemperatureC();
+            }
+
+            default -> throw new IllegalArgumentException(
+                    "Unknown sensor data type: " + sensorState.getData().getClass()
+            );
+        };
+    }
+
+    private boolean compare(
+            Object actualValue,
+            Integer expectedValue,
+            ConditionOperation operation
+    ) {
+        int actual = toInt(actualValue);
+
+        return switch (operation) {
+            case EQUALS -> actual == expectedValue;
+            case GREATER_THAN -> actual > expectedValue;
+            case LOWER_THAN -> actual < expectedValue;
+        };
+    }
+
+    private int toInt(Object value) {
+        if (value instanceof Boolean booleanValue) {
+            return booleanValue ? 1 : 0;
+        }
+
+        return ((Number) value).intValue();
     }
 }
